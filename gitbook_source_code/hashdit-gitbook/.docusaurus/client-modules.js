4c5e08237106077226cba38ca8c4cba75bd4151e
BarrySauce <masiteng1234@gmail.com>
2022-06-21 16:31:53 +0800

    Update .DS_Store

---
+++ gitbook_source_code/hashdit-gitbook/.docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require('/Users/barryma/work/hashdit-docs-main/node_modules/infima/dist/css/default/default.css'),
+  require('/Users/barryma/work/hashdit-docs-main/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/Users/barryma/work/hashdit-docs-main/node_modules/@docusaurus/theme-classic/lib/admonitions.css'),
+  require('/Users/barryma/work/hashdit-docs-main/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/Users/barryma/work/hashdit-docs-main/src/css/custom.css'),
+];
