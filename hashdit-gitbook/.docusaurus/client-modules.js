e4de3d408086af33e827bc0a3c27b8fdefb17927
BarrySauce <84897434+BarrySauce@users.noreply.github.com>
2022-06-21 16:34:15 +0800

    Delete hashdit-gitbook directory

--- hashdit-gitbook/.docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require('/Users/barryma/work/hashdit-docs-main/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/barryma/work/hashdit-docs-main/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/barryma/work/hashdit-docs-main/node_modules/@docusaurus/theme-classic/lib/admonitions.css'),
-  require('/Users/barryma/work/hashdit-docs-main/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/barryma/work/hashdit-docs-main/src/css/custom.css'),
-];
--- hashdit-gitbook/docs/diting-api/api-access.md
+++
@@ -1,8 +0,0 @@
----
-sidebar_position: 1
----
-
-# Access
-
-Please apply for access to our API through support@hashdit.io. 
-In your application, please state your usage scenario and estimated number of calls per day.
